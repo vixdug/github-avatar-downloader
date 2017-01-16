0c7570368a40b07b42cc40d7ec07fc5d08988490
vixdug <victoria.duggan@shopify.com>
2017-01-16 22:54:24 +0000

    updated request headers

---
+++ download_avatars.js
@@ -0,0 +1,41 @@
+var request = require('request');
+
+console.log('Github Avatar Downloader App');
+
+var GITHUB_USER = "vixdug";
+var GITHUB_TOKEN = "12ed0f76821ee1171ee6e389a6cf6c506f1ddccc"
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+ var requestURL = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+ var requestOptions = {
+   url: requestURL,
+   headers: {
+     'User-Agent': 'Github Avatar Project'
+   },
+   bearer: GITHUB_TOKEN
+ }
+
+ var fs = require('fs');
+
+ request.get(requestOptions)
+        .on('error', function (err) {
+          throw err;
+        })
+        .on('response', function (response) {
+          console.log('Response Status Code: ', response.statusCode, response.statusMessage, response.headers['content-type']);
+
+        })
+        .pipe(fs.createWriteStream('./contributors.js'));
+        console.log(requestURL);
+}
+
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
