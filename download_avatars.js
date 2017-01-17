35a5bfbfd4c80479d4a6861b7ff99dbee7371600
vixdug <victoria.duggan@shopify.com>
2017-01-17 02:37:40 +0000

    parsed JSON and extracted object keys

--- download_avatars.js
+++ download_avatars.js
@@ -1,41 +1,53 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Github Avatar Downloader App');
 
 var GITHUB_USER = "vixdug";
-var GITHUB_TOKEN = "12ed0f76821ee1171ee6e389a6cf6c506f1ddccc"
+var GITHUB_TOKEN = "1f9205487148b285508f6bc7f6e5aa2642dd4ef9"
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
- var requestURL = 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+ var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN +  '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
  var requestOptions = {
    url: requestURL,
    headers: {
      'User-Agent': 'Github Avatar Project'
    },
-   bearer: GITHUB_TOKEN
  }
 
- var fs = require('fs');
-
- request.get(requestOptions)
-        .on('error', function (err) {
-          throw err;
-        })
-        .on('response', function (response) {
-          console.log('Response Status Code: ', response.statusCode, response.statusMessage, response.headers['content-type']);
-
-        })
-        .pipe(fs.createWriteStream('./contributors.js'));
-        console.log(requestURL);
+ request(requestOptions, function (error, response, body) {
+     if (!error && response.statusCode == 200) {
+       var parsed = JSON.parse(body);
+       parsed.forEach(function(value, index){
+         var login = value.login;
+         var url = value.avatar_url;
+         console.log("This is the login:", login);
+         console.log("This is the avatar_url:", url);
+       });
+     }
+   });
 }
 
 
-
-
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
+})
+
+//         .on('error', function (err) {
+//           throw err;
+//           error = err;
+//
+//         })
+//         .on('response', function (response) {
+//
+//         })
+//         .on('end', function () {
+//         cb(error, result);
+//         console.log('the end of request');
+//           })
+//
+//         .pipe(fs.createWriteStream('./contributors'));
